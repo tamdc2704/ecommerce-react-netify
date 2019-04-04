49225375da7c485b49de2ff57a480facfd794d24
tamdc <dctam@ikornsolutions.com>
2019-04-04 17:03:16 +0700

    add css for img

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,11 +5,17 @@ import { ProductConsumer } from '../context'
 
 export default class Product extends Component {
   render() {
-    const { id, title, img, price, inCart } = this.props
-    
+    const { id, title, img, price, inCart } = this.props.product
+    console.log(img)
     return (
-      <ProductWrapper>
-        {this.props.product.title}
+      <ProductWrapper className='col-9 col-md-6 col-lg-3 mx-auto my-3'>
+        <div className="card">
+          <div className="img-container p-5">
+            <Link to='/detail'>
+              <img alt='product' src={img} className='card-img-top'/>
+            </Link>
+          </div>
+        </div>
       </ProductWrapper>
     );
   }
